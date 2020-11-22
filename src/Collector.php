10feeb88aee1be26f81959682a75a27607d437ac
Anton Perevoshchikov <anper3.5@gmail.com>
2020-11-22 15:14:39 +0300

    Removes auto register in collector

--- src/Collector.php
+++ src/Collector.php
@@ -2,8 +2,6 @@
 
 namespace Anper\Predis\CommandCollector;
 
-use Predis\ClientInterface;
-
 /**
  * Class Collector
  * @package Anper\Predis\CommandCollector
@@ -14,31 +12,6 @@ class Collector
      * @var Profile[]
      */
     protected $profiles = [];
-
-    /**
-     * @var ClientInterface
-     */
-    protected $client;
-
-    /**
-     * @param ClientInterface $client
-     *
-     * @throws Exception
-     */
-    public function __construct(ClientInterface $client)
-    {
-        register_collector($client, $this, true);
-
-        $this->client = $client;
-    }
-
-    /**
-     * @return ClientInterface
-     */
-    public function getClient(): ClientInterface
-    {
-        return $this->client;
-    }
 
     /**
      * @return Profile[]
--- tests/CollectorTest.php
+++ tests/CollectorTest.php
@@ -5,9 +5,6 @@ namespace Anper\Predis\CommandCollector\Tests;
 use Anper\Predis\CommandCollector\Collector;
 use Anper\Predis\CommandCollector\Profile;
 use PHPUnit\Framework\TestCase;
-use Predis\Client;
-
-use function Anper\Predis\CommandCollector\get_collectors;
 
 /**
  * Class CollectorTest
@@ -15,32 +12,12 @@ use function Anper\Predis\CommandCollector\get_collectors;
  */
 class CollectorTest extends TestCase
 {
-    public function testRegister(): void
-    {
-        $client = new Client();
-
-        $collector1 = new Collector($client);
-        $collector2 = new Collector($client);
-
-        $this->assertSame([$collector1, $collector2], get_collectors($client));
-    }
-
     public function testProfile(): void
     {
-        $client = new Client();
-
-        $collector = new Collector($client);
+        $collector = new Collector();
         $profile = $this->createMock(Profile::class);
         $collector->__invoke($profile);
 
         $this->assertEquals([$profile], $collector->getProfiles());
-    }
-
-    public function testGetClient(): void
-    {
-        $client = new Client();
-        $collector = new Collector($client);
-
-        $this->assertSame($client, $collector->getClient());
     }
 }
